0e55857ce1353fa81f0601eb66db13bb2917c7e8
Agustín Barcia <agustinbarcia.dev@gmail.com>
2024-11-22 21:00:30 -0300

    added russia back

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function CountriesPage() {
 
             {/* Countries List */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-                {countries.filter((country) => country.countryCode !== "RU").map((country) => (
+                {countries.map((country) => (
                     <Link
                         key={country.countryCode}
                         href={`/${country.countryCode}`}
